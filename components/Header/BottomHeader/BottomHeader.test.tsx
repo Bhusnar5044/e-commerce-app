c5a332108283f3c477c41fbe9a65d8a7c7dbc2d8
Pradip Bhusnar <bhusnarpradip@gmail.com>
2023-07-23 18:17:46 +0530

    PSB | added fetch mock setup

--- components/Header/BottomHeader/BottomHeader.test.tsx
+++ components/Header/BottomHeader/BottomHeader.test.tsx
@@ -1,9 +1,14 @@
 import { render } from '@test-utils';
+import { Suspense } from 'react';
 import { BottomHeader } from './BottomHeader';
 
 describe('BottomHeader', () => {
-    it('should render properly', () => {
-        const { container } = render(<BottomHeader />);
+    it('should render properly', async () => {
+        const { container } = render(
+            <Suspense>
+                <BottomHeader />
+            </Suspense>,
+        );
         expect(container).toMatchSnapshot();
     });
 });
--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,14 @@
 import '@testing-library/jest-dom';
 import '@testing-library/jest-dom/extend-expect';
+import { TextDecoder, TextEncoder } from 'util';
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
+
+require('jest-fetch-mock').enableMocks();
+
+jest.mock('next/config', () => () => ({
+    publicRuntimeConfig: {},
+}));
 
 jest.mock('next/navigation', () => ({
     ...require('next-router-mock'),
