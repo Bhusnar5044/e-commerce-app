9d0a3153c8bc023730318d89806f702f73ea1bcd
Pradip Bhusnar <bhusnarpradip@gmail.com>
2023-07-23 15:34:15 +0530

    PSB | updated style and logo

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -4,7 +4,7 @@ import { Container } from '../ui/Container/Container';
 
 export const Header = () => {
     return (
-        <div className="bg-blue-200 w-full h-20 bg-amazon_blue text-lightText fixed top-0 z-50">
+        <div className="bg-blue-200 w-full h-18 bg-amazon_blue text-lightText fixed top-0 z-50">
             <Container>
                 <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
                     {/* logo */}
@@ -12,7 +12,7 @@ export const Header = () => {
                         href={'/'}
                         className="px-2 border border-transparent hover:border-white cursor-pointer duration-300 flex items-center justify-center h-[70%]"
                     >
-                        <p className="font-bold text-xl">STORE</p>
+                        <p className="font-bold text-xl">E-COMMERCE-APP</p>
                     </Link>
 
                     {/* cart */}
